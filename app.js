b960e3492d39f9e57a652178051651ba3a25dba7
Minhaj <minhajbaig360@gmail.com>
2022-11-04 20:54:57 -0700

    Chapter 01 to 05

---
+++ app.js
@@ -0,0 +1,172 @@
+// Chapter 1  assignment
+
+
+// alert ("Error! Please enter a valid password.");
+// alert ("Welcome to JS Land... \n Happy Coding!");
+// alert ("Welcome to JS Land...");
+// alert (12345);
+// alert("Happy Coding!");
+
+
+// Chapter 2 assignment
+
+// var username="SMIT Student 26";
+// var myname="Minhaj Baig";
+// var message=("Hello World")
+// alert (username);
+// alert (myname);
+// alert (message)
+
+
+
+// var name=("Minhaj Baig");
+// var age=("23");
+// var course=("Certified Mobile Application Developer");
+
+// alert (name);
+// alert (age);
+// alert(course);
+
+
+// var pizza=("PIZZA \n PIZZ \n PIZ \n PI \n P");
+// alert (pizza);
+
+
+// Example of concatenation
+// var email="minhajbaig222@gmail.com"
+// alert ("My email address is " + email)
+
+// var  book=("A smarter way to learn javascript")
+// alert("I'm trying to learn from the Book " + book)
+
+
+// document.write("<h1>Yah! I can write HTML content through JAVASCRIPT</h1>")
+// document.write("<h2>Yah! I can write HTML content through JAVASCRIPT</h2>")
+// document.write("<h3>Yah! I can write HTML content through JAVASCRIPT</h3>")
+// document.write("<h4>Yah! I can write HTML content through JAVASCRIPT</h4>")
+// document.write("<h5>Yah! I can write HTML content through JAVASCRIPT</h5>")
+// document.write("<h6>Yah! I can write HTML content through JAVASCRIPT</h6>")
+
+
+// alert("▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬")
+
+// Chapter 2 assignment - Ends
+
+// Chapter 3 assignment - 1. Declare a variable called age & assign to it your age. Show your age in an alert box.
+
+// var age=23;
+// alert("I am " + age + " years old");
+
+// 2. Declare & initialize a variable to keep track of how many times a visitor has visited a web page. Show his/her number of visits on your web page. For example: “You have visited this site N times
+
+// var visits=14
+// alert("You have visited this site " + age + " times")
+
+// 3.Declare a variable called birthYear & assign to it your birth year. Show the following message in your browser
+
+// var birthyear = 1999
+// document.write("My birth year is " + birthyear + "<br>")
+// document.write("Data type of my declared variable is number")
+
+
+// 4. A visitor visits an online clothing store
+
+// name = prompt("Enter Your Name");
+// product = prompt("Enter Porduct Name");
+// quantity = parseInt(prompt("Enter Quantity", 1))
+
+// if (quantity === quantity) {
+//     document.write(name + " Ordered " + quantity + product + " on XYZ Clothing Store")
+// }
+// else {
+//     document.write("Please Enter Product Quantity")
+// }
+
+// Chapter 3 assignment - Ends
+
+// Chapter 4 assignment -1 Declare 3 variables in one statement
+
+    // var a = 2;
+    // var b = 4;
+    // var c = 6;
+    // var d = 12 ;
+    // document.write("the sum of  " + a + " + " + b + " + " + c + " + " + "is " + d); 
+
+// 2. Declare 5 legal & 5 illegal variable names.
+
+    // var letters = "Letter, digits, underscores, & $ sign is allowed ";
+    // var letters_1 = "Must began with a $, _ or a letter ";
+    // var $Sign = "Javascript reserved name cannot be served as a name";
+    // var $sign = "$Sign or $sign are different variables"
+    // var $SIGN = "These all are legal variables"
+    // document.write(letters + "<br>" + letters_1 + "<br>" + $Sign + "<br>" + $sign + "<br>" + $SIGN)
+
+    // illegal variables
+    // var 1illegal = "illegal variale"
+    // var var = "illegal variable"
+    // var ILL*EGAL = "illegal"
+    // var ILLE+GAL = "illegal"
+    // var %illegal = "illegal"
+
+    // Chapter 4 assignment - Ends
+
+    // Chapter 5 assignment - 1. Write a program that take two numbers & add them in a new variable. Show the result in your browser - 2. Repeat task1 for subtraction, multiplication, division & modulus
+
+
+    // var numb1 = parseInt (prompt("Enter Number 1", "0"))
+    // var numb2 = parseInt (prompt("Enter Number 2", "0"))
+    // var operation = prompt("Enter Operation")
+    // // var sum = numb1+numb2  // you can also write it here
+
+    // if(operation == "+"){
+    //     var sum = numb1+numb2
+    //     document.write("Sum of " + numb1 + " & " + numb2 + " is " + sum)
+    // }
+    // else if(operation == "-"){
+    //     var sum = numb1-numb2
+    //     document.write("Subtraction of " + numb1 + " & " + numb2 + " is " + sum)
+    // }
+    // else if(operation == "/"){
+    //     var sum = numb1/numb2
+    //     document.write("Division of " + numb1 + " & " + numb2 + " is " + sum)
+    // }
+    // else if(operation == "%"){
+    //     var sum = numb1%numb2
+    //     document.write("Modulus of " + numb1 + " & " + numb2 + " is " + sum)
+    // }
+    // else{
+    //     document.write("You entered an invalid number")
+    // }
+
+
+    //  3. Do the following using JS Mathematic Expressions 
+    // a. Declare a variable. 
+    // b. Show the value of variable in your browser like “Value after variable declaration is: ??”. 
+    // c. Initialize the variable with some number.
+    // d. Show the value of variable in your browser like “Initial value: 5”. 
+    // e. Increment the variable. 
+    // f. Show the value of variable in your browser like “Value after increment is: 6”. 
+    // g. Add 7 to the variable. 
+    // h. Show the value of variable in your browser like “Value after addition is: 13”. 
+    // i. Decrement the variable. 
+    // j. Show the value of variable in your browser like “Value after decrement is: 12”. 
+    // k. Show the remainder after dividing the variable’s value by 3.
+    // l. Output : “The remainder is : 0”.
+
+
+        var value1 = undefined
+        document.write(" Value after variable declaration is " + value1 + "<br>");
+
+        var value1 = 5
+        document.write(" Initial value: " + value1 + "<br>");
+        
+        document.write("Value after increment is: " + ++value1 + "<br>")
+
+        var value1 = value1 + 7
+        document.write("Value after addition is:" + value1 + "<br>")
+
+        document.write("Value after decrement is: " + --value1 + "<br>")
+
+        document.write("The remainder is " + value1 % 3)
+
+
